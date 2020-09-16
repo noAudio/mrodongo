634d72207c9fd1996627482371480dea970e665c
Samuel Odongo <samieodd@gmail.com>
2020-09-16 23:20:16 +0300

    Made every view pass in a page title

--- app/views.py
+++ app/views.py
@@ -1,18 +1,43 @@
 from app import app
 from flask import render_template
 
+nav_links = {
+    'Home': '/',
+    'About': '/about',
+    'Projects': '/projects',
+    'Contact': '/contact',
+    'Blog': '/blog',
+}
+
+skills = ['Python', 'SQL', 'HTML/CSS/JS', 'Bootstrap']
+
+weekend = ('Saturday', 'Sunday')
+
 @app.route('/')
 def index():
-    return render_template('public/index.html')
+    page_title = 'Home'
+    return render_template('public/index.html', page_title=page_title, nav_links=nav_links)
 
 @app.route('/about')
 def about():
-    return render_template('public/about.html')
+    page_title = 'About'
+    return render_template('public/about.html', page_title=page_title, skills=skills, nav_links=nav_links)
 
 @app.route('/contact')
 def contact():
-    return render_template('public/contact.html')
+    page_title = 'Contact'
+    return render_template('public/contact.html', page_title=page_title, nav_links=nav_links)
 
 @app.route('/test_route')
 def test_route():
-    return render_template('public/unknown_error.html')
+    return render_template('public/unknown_error.html', nav_links=nav_links)
+
+
+# from datetime import datetime
+# day_today = datetime.today().strftime('%A')
+# weekend = ('Saturday', 'Wednesday')
+# print(day_today)
+# if day_today in weekend:
+#     print('Its the weekend baby!')
+# else:
+#     print('Weekend, it is not')
