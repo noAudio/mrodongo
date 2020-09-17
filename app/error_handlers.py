472357b175ce59ed218e192c3fa4f917e6781c26
Samuel Odongo <samieodd@gmail.com>
2020-09-17 15:52:17 +0300

    Created error handlers and imported them into app

---
+++ app/error_handlers.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template, request
+from app.views import nav_links
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('public/error_404.html', nav_links=nav_links)
+
+@app.errorhandler(401)
+def error_401(e):
+    return render_template('public/denied.html', nav_links=nav_links)
+
+@app.errorhandler(400)
+def error_401(e):
+    app.logger.error(f'Server error ({e}), at {request.url}')
+    return render_template('public/unexpected_error.html', nav_links=nav_links)
+
+@app.errorhandler(500)
+def error_401(e):
+    app.logger.error(f'Server error ({e}), at {request.url}')
+    return render_template('public/unexpected_error.html', nav_links=nav_links)
