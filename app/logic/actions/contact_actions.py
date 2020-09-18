b92f7f187082ab4920b266e5bc0081213ec0dbdd
Mr. Odongo <samieodd@gmail.com>
2020-09-18 17:54:08 +0300

    Adjusted vars

--- app/logic/actions/contact_actions.py
+++ app/logic/actions/contact_actions.py
@@ -1,17 +1,18 @@
 import smtplib
 from email.message import EmailMessage
-from email_config import sender, receiver, subject, usrnm, pwd
+from typing import Any
+from email_config import email_attr, usrnm, pwd
 
-def create_email(name, contact, message):
+def create_email(name: str, contact: str, message: str):
     email = EmailMessage()
-    email['from'] = sender
-    email['to'] = receiver
-    email['subject'] = subject
+    email['from'] = email_attr['sender']
+    email['to'] = email_attr['receiver']
+    email['subject'] = email_attr['subject']
     email.set_content(f'Message from: {name} \n\nContact: {contact} \n\nMessage:\n{message}')
     print(email)
     return email
 
-def send_email(email):
+def send_email(email: Any):
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
